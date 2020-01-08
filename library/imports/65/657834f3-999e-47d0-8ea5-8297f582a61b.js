bc27a4e20154831a31a03cc33f80b61e8a354202
潘梦琳 <panmenglin@icourt.cc>
2020-01-08 17:41:45 +0800

    小游戏项目初始化

---
+++ library/imports/65/657834f3-999e-47d0-8ea5-8297f582a61b.js
@@ -0,0 +1,164 @@
+"use strict";
+cc._RF.push(module, '65783TzmZ5H0I6lgpf1gqYb', 'player');
+// scripts/player.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 主角跳跃高度
+        jumpHeight: 0,
+        // 主角跳跃持续时间
+        jumpDuration: 0,
+        // 最大移动速度
+        maxMoveSpeed: 0,
+        // 加速度
+        accel: 0,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        // 跳跃音效资源
+        jumpAudio: {
+            default: null,
+            type: cc.AudioClip
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        // 初始化跳跃动作
+        // this.jumpAction = this.setJumpAction();
+        // this.node.runAction(this.jumpAction);
+        this.jumpCount = 0;
+        console.log(this.node.y);
+
+        // 加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        // 主角当前水平方向速度
+        this.xSpeed = 0;
+
+        // 初始化键盘输入监听
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+    start: function start() {},
+    update: function update(dt) {
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // 限制主角的速度不能超过最大值
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // 根据当前速度更新主角的位置
+        this.node.x += this.xSpeed * dt;
+    },
+    onDestroy: function onDestroy() {
+        // 取消键盘输入监听
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+
+    setJumpAction: function setJumpAction() {
+        // 控制二段跳
+        if (this.jumpCount >= 2) {
+            return;
+        }
+
+        // 二段跳中止前一个动作
+        if (this.jumpAction) {
+            this.node.stopAction(this.jumpAction);
+        }
+
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+
+        // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        var callback = cc.callFunc(this.jumpDownAction, this);
+
+        // 不断重复
+        // return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+
+        this.jumpCount += 1;
+        this.jumpAction = cc.sequence(jumpUp, callback);
+        this.node.runAction(this.jumpAction);
+    },
+    jumpDownAction: function jumpDownAction() {
+        // 下落
+        var jumpDownHeight = this.node.y + 120;
+        var jumpDownTime = jumpDownHeight / this.jumpHeight * this.jumpDuration;
+        var jumpDown = cc.moveBy(jumpDownTime, cc.v2(0, -jumpDownHeight)).easing(cc.easeCubicActionIn());
+
+        this.playJumpSound();
+        var callback = cc.callFunc(this.jumpDownEnd, this);
+        this.jumpAction = cc.sequence(jumpDown, callback);
+        this.node.runAction(this.jumpAction);
+    },
+    playJumpSound: function playJumpSound() {
+        // 调用声音引擎播放声音
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+    jumpDownEnd: function jumpDownEnd() {
+        this.jumpCount = 0;
+        this.jumpAction = null;
+    },
+    onKeyDown: function onKeyDown(event) {
+        // set a flag when key pressed
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = true;
+                break;
+            case cc.macro.KEY.s:
+                this.setJumpAction();
+                break;
+        }
+    },
+    onKeyUp: function onKeyUp(event) {
+        // unset a flag when key released
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = false;
+                break;
+        }
+    }
+});
+
+cc._RF.pop();
---
+++ temp/quick-scripts/assets/scripts/game.js
@@ -0,0 +1,157 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/game.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, 'd202b2zG6tLcY/eOQcr8Oox', 'game', __filename);
+// scripts/game.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 这个属性引用了星星预制资源
+        starPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        // 星星产生后消失时间的随机范围
+        maxStarDuration: 0,
+        minStarDuration: 0,
+        // 地面节点，用于确定星星生成的高度
+        ground: {
+            default: null,
+            type: cc.Node
+        },
+        // player 节点，用于获取主角弹跳的高度，和控制主角行动开关
+        player: {
+            default: null,
+            type: cc.Node
+        },
+        // score label 的引用
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        // 得分音效资源
+        scoreAudio: {
+            default: null,
+            type: cc.AudioClip
+        },
+        bg1: null,
+        bg2: null,
+        curBg: null,
+        bgSpeed: 10
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        // this.score = 0;
+
+        // 获取地平面的 y 轴坐标
+        this.groundY = this.ground.y + this.ground.height / 2;
+        // // 生成一个新的星星
+        // this.spawnNewStar();
+
+
+        // 初始化计时器
+        this.timer = 0;
+        this.starDuration = 0;
+        // 生成一个新的星星
+        this.spawnNewStar();
+        // 初始化计分
+        this.score = 0;
+
+        this.bg1 = this.node.getChildByName('background1');
+        this.bg2 = this.node.getChildByName('background2');
+        this.cur_bg = this.bg1;
+
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEndCallback, this, true);
+    },
+    start: function start() {},
+    update: function update(dt) {
+        if (this.timer > this.starDuration) {
+            // this.gameOver();
+            // return;
+        }
+        this.timer += dt;
+
+        var s = dt * this.bgSpeed;
+        this.bg1.x -= s;
+        this.bg2.x -= s;
+
+        if (this.cur_bg.x <= -1360) {
+            // 地图切换
+            if (this.cur_bg == this.bg2) {
+                this.bg2.x = this.bg1.x + 1360;
+                this.cur_bg = this.bg1;
+            } else {
+                this.bg1.x = this.bg2.x + 1360;
+                this.cur_bg = this.bg2;
+            }
+        }
+    },
+
+
+    spawnNewStar: function spawnNewStar() {
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.starPrefab);
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+        newStar.setPosition(this.getNewStarPosition());
+
+        newStar.getComponent('star').game = this;
+
+        this.starDuration = this.minStarDuration + Math.random() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
+    },
+
+    getNewStarPosition: function getNewStarPosition() {
+        var randX = 0;
+        // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
+        var randY = this.groundY + Math.random() * this.player.getComponent('player').jumpHeight + 50;
+        // 根据屏幕宽度，随机得到一个星星 x 坐标
+        var maxX = this.node.width / 2;
+        randX = (Math.random() - 0.5) * 2 * maxX;
+        // 返回星星坐标
+        return cc.v2(randX, randY);
+    },
+    gainScore: function gainScore() {
+        this.score += 1;
+        // 更新 scoreDisplay Label 的文字
+        this.scoreDisplay.string = 'Score: ' + this.score;
+        // 播放得分音效
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    },
+    gameOver: function gameOver() {
+        this.player.stopAllActions(); //停止 player 节点的跳跃动作
+        cc.director.loadScene('game');
+    },
+    onTouchEndCallback: function onTouchEndCallback() {
+        this.player.getComponent('player').setJumpAction();
+    }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=game.js.map
+        
